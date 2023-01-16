1f74b33ceb422bca015f5a4019f4940c960628a8
Antonio La Chira <antonio1891@hotmail.com>
2023-01-16 16:17:32 +0100

    Added max_vis var, changed initial placement on grid

--- evac_model.py
+++ evac_model.py
@@ -10,19 +10,19 @@ from person_agent import PersonAgent
 class EvacModel(mesa.Model):
     def __init__(self, N, width, height):
         self.num_agents = N
+        # Default maximum visible distance
+        self.max_vis = 5
         # Activate all agents in random order each step
         self.schedule = mesa.time.RandomActivation(self)
         self.grid = mesa.space.SingleGrid(width, height, False)
 
         # Initialize agents in a loop probably
         for uid in range(self.num_agents):
-            a = PersonAgent(uid, self)
-            self.schedule.add(a)
+            p = PersonAgent(uid, self)
+            self.schedule.add(p)
 
-            # Place agent in grid
-            x = self.random.randrange(self.grid.width)
-            y = self.random.randrange(self.grid.height)
-            self.grid.place_agent(a, (x, y))
+            # Randomly place agent in grid
+            self.grid.position_agent(p, 'random')
 
     def step(self):
         # Scheduler will execute every agent's step() method
