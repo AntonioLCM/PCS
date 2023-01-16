c1ccaf243694b39795b62305c9378f461060e695
Antonio La Chira <antonio1891@hotmail.com>
2023-01-16 16:18:50 +0100

    Grid 100 x 100, with 100 agents

--- evac_model_viz.py
+++ evac_model_viz.py
@@ -19,9 +19,9 @@ def agent_portrayal(agent):
     return portrayal
 
 
-grid = mesa.visualization.CanvasGrid(agent_portrayal, 10, 10, 500, 500)
+grid = mesa.visualization.CanvasGrid(agent_portrayal, 100, 100, 700, 700)
 server = mesa.visualization.ModularServer(
-    EvacModel, [grid], "Evacuation Model", {"N": 10, "width": 10, "height": 10}
+    EvacModel, [grid], "Evacuation Model", {"N": 100, "width": 100, "height": 100}
 )
 server.port = 8521  # The default
 server.launch()
