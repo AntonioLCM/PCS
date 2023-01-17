b417201636d7bc1494ea64ee2a7a839dd9d6f26b
Antonio La Chira <antonio1891@hotmail.com>
2023-01-17 14:25:19 +0100

    Add specific portrayals for agents, changed gridsize and num_agents

--- evac_model_viz.py
+++ evac_model_viz.py
@@ -5,23 +5,34 @@
 
 import mesa
 from evac_model import EvacModel
+from wall_agent import WallAgent
+from person_agent import PersonAgent
 
 
 # Placeholder...
 def agent_portrayal(agent):
     portrayal = {
-        "Shape": "circle",
         "Filled": "true",
         "Layer": 0,
         "Color": "red",
-        "r": 0.5,
     }
+
+    if isinstance(agent, WallAgent):
+        portrayal["Color"] = "black"
+        portrayal["Shape"] = "rect"
+        portrayal["w"] = 1
+        portrayal["h"] = 1
+
+    if isinstance(agent, PersonAgent):
+        portrayal["Shape"] = "circle"
+        portrayal["r"] = .5
+
     return portrayal
 
 
-grid = mesa.visualization.CanvasGrid(agent_portrayal, 100, 100, 700, 700)
+grid = mesa.visualization.CanvasGrid(agent_portrayal, 10, 10, 500, 500)
 server = mesa.visualization.ModularServer(
-    EvacModel, [grid], "Evacuation Model", {"N": 100, "width": 100, "height": 100}
+    EvacModel, [grid], "Evacuation Model", {"N": 5, "width": 10, "height": 10}
 )
 server.port = 8521  # The default
 server.launch()
