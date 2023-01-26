009cb2626175d138d61196f50e4eda25c7ac4e5f
Antonio La Chira <antonio1891@hotmail.com>
2023-01-26 16:26:20 +0100

    Add possibility for agentcount vs iter chart visualization

--- evac_model_viz.py
+++ evac_model_viz.py
@@ -33,8 +33,12 @@ def agent_portrayal(agent):
 
 
 grid = mesa.visualization.CanvasGrid(agent_portrayal, 151, 200, 800, 800)
+
+# chart = mesa.visualization.ChartModule([{"Label": "agent_count",
+#                                          "Color": "Black"}],
+#                                        data_collector_name='datacollector')
 server = mesa.visualization.ModularServer(
-    EvacModel, [grid], "Evacuation Model", {"N": 500, "width": 151, "height": 200}
+    EvacModel, [grid], "Evacuation Model", {"N": 5000, "width": 151, "height": 200}
 )
 server.port = 8521
 server.launch()
