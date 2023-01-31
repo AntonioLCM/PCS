ed9179879d95582433ee6189eab0e6c960368cdb
Antonio La Chira <antonio1891@hotmail.com>
2023-01-31 19:18:25 +0100

    Changed imports, gen_ar_list now takes exits

--- generate_arrow_list.py
+++ generate_arrow_list.py
@@ -4,8 +4,7 @@ project: Project Smoke
 author: Abel John Oakley
 """
 
-from Blueprints.arrow_priority import *
-from person_agent import ALLEXITS
+import Blueprints.arrow_priority as arp
 from arrow_class import Arrow
 
 
@@ -24,11 +23,12 @@ arrow_list = [(33, 47, 2), (32, 70, 3), (32, 94, 0),
               (126, 32, 3), (102, 22, 3), (90, 22, 1),
               (56, 33, 3), (46, 33, 1)]
 
-def generate_arrow_list():
+
+def generate_arrow_list(exits):
     """
-    
+
     """
-    arrows =[]
-    for (x,y,d) in arrow_list:
-        arrows.append(Arrow((x,y),d, exit_priority((x,y), ALLEXITS)))
+    arrows = []
+    for (x, y, d) in arrow_list:
+        arrows.append(Arrow((x, y), d, arp.exit_priority((x, y), exits)))
     return arrows
