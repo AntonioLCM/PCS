6dca268180d379a90634636206d2fc89bee42480
FIRST_NAME LAST_NAME <berkayyayla98@gmail.com>
2023-01-31 17:35:58 +0100

    des

--- arrow_class.py
+++ arrow_class.py
@@ -1,3 +1,9 @@
+"""
+    Team: Eight minus one
+    Description: A class for 'arrows' to be used in an abm within
+                 Mesa.
+"""
+
 class Arrow:
     def __init__(self, pos, dir, exit_priority):
         self.pos = None
