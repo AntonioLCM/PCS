316cdf007179f94840491eb5978a329d4cf6955a
Antonio La Chira <antonio1891@hotmail.com>
2023-01-31 14:44:56 +0100

    Formatting

--- Blueprints/arrow_priority.py
+++ Blueprints/arrow_priority.py
@@ -4,23 +4,22 @@ project: Project Smoke
 author: Abel John Oakley
 """
 
-import numpy as np
-
 
 def arrow_priority(exit_priority):
     """
     function: this function takes a list with as first element
     the location of an arrow in tuple form, and as second element
     the closest exit. It then calculates the distance between
-    the two. 
+    the two.
     input: exit_priority [(n,m), (x, y)]
     output: (x,y,distance)
     """
-    [(x,y), (n,m)] = exit_priority                      
-    
+    [(x, y), (n, m)] = exit_priority
+
     distance = abs(x - n) + abs(y - m)                  # manhatten distance
 
-    return (x,y,distance)
+    return (x, y, distance)
+
 
 def exit_priority(arrow, exits):
     """
@@ -30,12 +29,12 @@ def exit_priority(arrow, exits):
     input: arrow (n,m), exits [(x1,y1), (x2,y2)]
     return: exit_priority [(n,m), (x, y)]
     """
-    dis = 10000                                         # set large distance
-    for x,y in exits:                                   # loop through possible exits
-        n,m = arrow
-        new_dis = abs(x - n) + abs(y - m)               # find manhatten distance
-        if new_dis < dis:                               # check shortest distance
+    dis = 10000                                 # set large distance
+    for x, y in exits:                          # loop through possible exits
+        n, m = arrow
+        new_dis = abs(x - n) + abs(y - m)       # find manhatten distance
+        if new_dis < dis:                       # check shortest distance
             dis = new_dis
-            exit_priority = [(n,m), (x,y)]              # save both arrow and exit
+            exit_priority = [(n, m), (x, y)]    # save both arrow and exit
 
     return exit_priority
