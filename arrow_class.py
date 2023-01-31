80901b1ac58d3c685dd4a59f8671ee7ca4d06987
Antonio La Chira <antonio1891@hotmail.com>
2023-01-31 19:42:06 +0100

    Method

--- arrow_class.py
+++ arrow_class.py
@@ -4,6 +4,8 @@
                  Mesa.
 """
 
+import numpy as np
+
 
 class Arrow:
 
@@ -12,5 +14,8 @@ class Arrow:
         self.dir = direction
         self.exit_priority = exit_priority
 
-    def _is_in_range(self, dist):
-        pass
+    def is_in_range(self, loc, dist):
+        x, y = loc
+        a, b = self.pos
+        return np.floor(np.sqrt(np.square(x - a)
+                                + np.square(y - b))) <= dist
