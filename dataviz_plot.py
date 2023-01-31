2fd025e59d69ddea712b2695f6936507ff7e3497
FIRST_NAME LAST_NAME <berkayyayla98@gmail.com>
2023-01-31 13:44:17 +0100

    dataviz avg

--- dataviz_plot.py
+++ dataviz_plot.py
@@ -14,7 +14,7 @@ plt.plot(data["Exit3"], label="Exit3")
 plt.plot(data["Exit4"], label="Exit4")
 total_exits = data["Exit1"] + data["Exit2"] + data["Exit3"] + data["Exit4"]
 avg_exits = total_exits / 4
-plt.plot(avg_exits, label="Average Exits")
+plt.plot(avg_exits, label="TotalAmountPeopleLeaving")
 plt.legend()
 plt.xlabel("Step")
 plt.ylabel("Number of People Exiting")
