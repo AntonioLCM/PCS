1f69b2b6320e7399fe4186ba1c278b7acca593be
Antonio La Chira <antonio1891@hotmail.com>
2023-01-31 18:59:03 +0100

    Formatting, function idea

--- arrow_class.py
+++ arrow_class.py
@@ -4,8 +4,13 @@
                  Mesa.
 """
 
+
 class Arrow:
-    def __init__(self, pos, dir, exit_priority):
-        self.pos = None
-        self.dir = None
-        self.exit_priority = None
+
+    def __init__(self, pos, direction, exit_priority):
+        self.pos = pos
+        self.dir = direction
+        self.exit_priority = exit_priority
+
+    def _is_in_range(self, dist):
+        pass
