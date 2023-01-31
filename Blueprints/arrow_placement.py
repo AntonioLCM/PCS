27d0903ab9356e07d059b37e388e29b2d6756e38
Abel John <abel.oakley@student.uva.nl>
2023-01-31 12:18:52 +0100

    arrow list

---
+++ Blueprints/arrow_placement.py
@@ -0,0 +1,32 @@
+"""
+file name: arrow_placement.py
+project: Project Smoke
+author: Abel John Oakley
+"""
+
+from arrow_priority import*
+
+arrow_list = [(17, 25, 1)]
+
+
+"""
+arrow       = (x, y, direction, distance)
+x,y         = coordinates of the arrow
+direction   =   0: north
+                1: east
+                2: south
+                3: west
+distance    = distance towards the exit
+"""
+arrow_list = [(33,47, 2), (32,70,3), (32,94,0), 
+              (74,120,1), (80,120,3), (168,128,0), 
+              (168,102,0), (168,95,2),(168,78,1), 
+              (126,32, 3),(102,22,3), (90,22,1), 
+              (56,33, 3), (46,33,1)]
+
+def generate_arrow_list():
+    """
+    
+    """
+
+    return 0
