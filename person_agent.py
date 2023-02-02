43f130b5ede16d215172d8180ffceebf834fa6e4
Antonio La Chira <antonio1891@hotmail.com>
2023-02-02 06:37:53 +0100

    New exits, pf.()

--- person_agent.py
+++ person_agent.py
@@ -10,25 +10,27 @@ from Blueprints.arrow_priority import arrow_priority
 import a_star as pf
 
 
-EXIT1 = [(70, 18), (71, 18), (72, 18), (73, 18)]
+EXIT1 = [(70, 29), (71, 29), (72, 29), (73, 29)]
 EXIT2 = [(61, 33), (62, 33)]
 EXIT3 = [(79, 158), (80, 158), (81, 158), (82, 158), (83, 158)]
-EXIT4 = [(76, 181), (77, 181), (78, 181), (79, 181)]
+EXIT4 = [(76, 171), (77, 171), (78, 171), (79, 171)]
 ALL_EXITS = EXIT1 + EXIT2 + EXIT3 + EXIT4
 
 ARROWS = generate_arrow_list(ALL_EXITS)
 
 
 class PersonAgent(mesa.Agent):
-    def __init__(self, unique_id, model):
+    def __init__(self, unique_id, model, pos, end):
         # Initialize as Agent from mesa
         super().__init__(unique_id, model)
 
         # Define state etc. below
+        self.pos = pos
+        self.end = end
         self.dir = None
         self.arrows = ARROWS
         self.best_ar = None
-        self.path = pf.main()
+        self.path = pf.main(list(self.pos), list(self.end))
 
     def _sees_exit(self):
         # Check if there is an exit in neighborhood with range max_vis (visible
@@ -43,7 +45,7 @@ class PersonAgent(mesa.Agent):
         # of arrow.
         # self.model.grid.move_agent(self, next_loc)
 
-        next_loc = self.path.pop(0)[0]
+        next_loc = self.path.pop(0)
         self.model.grid.move_agent(self, next_loc)
 
     def pos_by_dir(self, direction):
