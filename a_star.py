1096e779accf39bd8bcbcd568ebbcd7eb175fe88
Antonio La Chira <antonio1891@hotmail.com>
2023-02-02 15:46:33 +0100

    Cleanup

--- a_star.py
+++ a_star.py
@@ -1,5 +1,4 @@
-
-from Blueprints.image_to_array import *
+from Blueprints.image_to_array import image_to_array
 import numpy as np
 
 
@@ -20,23 +19,30 @@ class Node:
         self.g = 0
         self.h = 0
         self.f = 0
+
     def __eq__(self, other):
         return self.position == other.position
 
-#This function return the path of the search
-def return_path(current_node,maze):
+
+# This function return the path of the search
+def return_path(current_node, maze):
     path = []
     no_rows, no_columns = np.shape(maze)
-    # here we create the initialized result maze with -1 in every position
+
+    # Here we create the initialized result maze with -1 in every position
     result = [[-1 for i in range(no_columns)] for j in range(no_rows)]
     current = current_node
+
     while current is not None:
         path.append(current.position)
         current = current.parent
+
     # Return reversed path as we need to show from start to end path
     path = path[::-1]
     start_value = 0
-    # we update the path of start to end found by A-star serch with every step incremented by 1
+
+    # We update the path of start to end found by A-star serch with every step
+    # incremented by 1
     for i in range(len(path)):
         result[path[i][0]][path[i][1]] = start_value
         start_value += 1
@@ -45,7 +51,8 @@ def return_path(current_node,maze):
 
 def search(maze, cost, start, end):
     """
-        Returns a list of tuples as a path from the given start to the given end in the given maze
+        Returns a list of tuples as a path from the given start to the given
+        end in the given maze
         :param maze:
         :param cost
         :param start:
@@ -60,57 +67,61 @@ def search(maze, cost, start, end):
     end_node.g = end_node.h = end_node.f = 0
 
     # Initialize both yet_to_visit and visited list
-    # in this list we will put all node that are yet_to_visit for exploration. 
+    # in this list we will put all node that are yet_to_visit for exploration.
     # From here we will find the lowest cost node to expand next
-    yet_to_visit_list = []  
-    # in this list we will put all node those already explored so that we don't explore it again
-    visited_list = [] 
-    
+    yet_to_visit_list = []
+
+    # In this list we will put all node those already explored so that we don't
+    # explore it again
+    visited_list = []
+
     # Add the start node
     yet_to_visit_list.append(start_node)
-    
-    # Adding a stop condition. This is to avoid any infinite loop and stop 
+
+    # Adding a stop condition. This is to avoid any infinite loop and stop
     # execution after some reasonable number of steps
     outer_iterations = 0
     max_iterations = (len(maze) // 2) ** 10
 
-    # what squares do we search . serarch movement is left-right-top-bottom 
-    #(4 movements) from every positon
+    # Which squares do we search. serarch movement is left-right-top-bottom
+    # (4 movements) from every positon
 
-    move  =  [[-1, 0 ], # go up
-              [ 0, -1], # go left
-              [ 1, 0 ], # go down
-              [ 0, 1 ]] # go right
-
+    move = [[-1, 0],  # go up
+            [0, -1],  # go left
+            [1, 0],  # go down
+            [0, 1]]  # go right
 
     """
-        1) We first get the current node by comparing all f cost and selecting the lowest cost node for further expansion
-        2) Check max iteration reached or not . Set a message and stop execution
-        3) Remove the selected node from yet_to_visit list and add this node to visited list
-        4) Perofmr Goal test and return the path else perform below steps
+        1) We first get the current node by comparing all f cost and selecting
+           the lowest cost node for further expansion
+        2) Check max iteration reached or not . Set a message and stop
+           execution
+        3) Remove the selected node from yet_to_visit list and add this node to
+           visited list
+        4) Perform Goal test and return the path else perform below steps
         5) For selected node find out all children (use move to find children)
-            a) get the current postion for the selected node (this becomes parent node for the children)
-            b) check if a valid position exist (boundary will make few nodes invalid)
+            a) get the current postion for the selected node (this becomes
+               parent node for the children)
+            b) check if a valid position exist (boundary will make few nodes
+               invalid)
             c) if any node is a wall then ignore that
             d) add to valid children node list for the selected parent
-            
+
             For all the children node
                 a) if child in visited list then ignore it and try next node
                 b) calculate child node g, h and f values
                 c) if child in yet_to_visit list then ignore it
                 d) else move the child to yet_to_visit list
     """
-    #find maze has got how many rows and columns 
+    # Find maze has got how many rows and columns
     no_rows, no_columns = np.shape(maze)
-    
-    # Loop until you find the end
-    
-    while len(yet_to_visit_list) > 0:
-        
-        # Every time any node is referred from yet_to_visit list, counter of limit operation incremented
-        outer_iterations += 1    
 
-        
+    # Loop until you find the end
+    while len(yet_to_visit_list) > 0:
+        # Every time any node is referred from yet_to_visit list, counter of
+        # limit operation incremented
+        outer_iterations += 1
+
         # Get the current node
         current_node = yet_to_visit_list[0]
         current_index = 0
@@ -118,34 +129,32 @@ def search(maze, cost, start, end):
             if item.f < current_node.f:
                 current_node = item
                 current_index = index
-                
-        # if we hit this point return the path such as it may be no solution or 
+
+        # If we hit this point return the path such as it may be no solution or
         # computation cost is too high
         if outer_iterations > max_iterations:
-            print ("giving up on pathfinding too many iterations")
-            return return_path(current_node,maze)
+            print("giving up on pathfinding too many iterations")
+            return return_path(current_node, maze)
 
         # Pop current node out off yet_to_visit list, add to visited list
         yet_to_visit_list.pop(current_index)
         visited_list.append(current_node)
 
-        # test if goal is reached or not, if yes then return the path
+        # Test if goal is reached or not, if yes then return the path
         if current_node == end_node:
-            return return_path(current_node,maze)
+            return return_path(current_node, maze)
 
         # Generate children from all adjacent squares
         children = []
 
-        for new_position in move: 
-
+        for new_position in move:
             # Get node position
-            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+            node_position = (current_node.position[0] + new_position[0],
+                             current_node.position[1] + new_position[1])
 
             # Make sure within range (check if within maze boundary)
-            if (node_position[0] > (no_rows - 1) or 
-                node_position[0] < 0 or 
-                node_position[1] > (no_columns -1) or 
-                node_position[1] < 0):
+            if (node_position[0] > (no_rows - 1) or node_position[0] < 0
+               or node_position[1] > (no_columns - 1) or node_position[1] < 0):
                 continue
 
             # Make sure walkable terrain
@@ -160,69 +169,56 @@ def search(maze, cost, start, end):
 
         # Loop through children
         for child in children:
-            
             # Child is on the visited list (search entire visited list)
-            if len([visited_child for visited_child in visited_list if visited_child == child]) > 0:
+            if len([visited_child for visited_child in visited_list
+                    if visited_child == child]) > 0:
                 continue
 
             # Create the f, g, and h values
             child.g = current_node.g + cost
-            ## Heuristic costs calculated here, this is using eucledian distance
-            child.h = (((child.position[0] - end_node.position[0]) ** 2) + 
-                       ((child.position[1] - end_node.position[1]) ** 2)) 
 
+            # Heuristic costs calculated here, this is using eucledian distance
+            child.h = (((child.position[0] - end_node.position[0]) ** 2) +
+                       ((child.position[1] - end_node.position[1]) ** 2))
             child.f = child.g + child.h
 
-            # Child is already in the yet_to_visit list and g cost is already lower
-            if len([i for i in yet_to_visit_list if child == i and child.g > i.g]) > 0:
+            # Child is already in the yet_to_visit list and g cost is already
+            # lower
+            if len([i for i in yet_to_visit_list
+                    if child == i and child.g > i.g]) > 0:
                 continue
 
             # Add the child to the yet_to_visit list
             yet_to_visit_list.append(child)
 
-def main(start, end):
-    #poppetje1 = [(35,12), (73,18)]
-    #poppetje2 = [(34,8), (73,18)]
-    #poppetje? = [(137, 183), (80,158)]
-    #poppetje3 = [(106, 82), (18,71)]
 
-    #poppetje4 = [(140, 166), (181,77)]
-    #poppetje5 = [(63, 157), (181,77)]
-    #poppetje6 = [(187,105), (181,77)]
-    #poppetje7 = [(57,140), (181,77)]
-    #poppetje8 = [(58,105), (181,77)]
-    #poppetje9 = [(70,105), (181,77)]
-    #poppetje10 = [(93, 19), (18,71)]
-    #poppetje11 = [(44,83), (18,71)]
-    #poppetje12 = [(94,37), (181,77)]
-    #poppetje13 = [(128,16), (18,71)]
-    #poppetje14 = [(36,16), (181,77)]
-    #poppetje15 = [(68,17), (18,71)]
-    #poppetje16 = [(188,16), (18,71)]
-    #poppetje17 = [(70,45), (18,71)]
+def main(start, end):
+    # poppetje1 = [(35,12), (73,18)]
+    # poppetje2 = [(34,8), (73,18)]
+    # poppetje? = [(137, 183), (80,158)]
+    # poppetje3 = [(106, 82), (18,71)]
+
+    # poppetje4 = [(140, 166), (181,77)]
+    # poppetje5 = [(63, 157), (181,77)]
+    # poppetje6 = [(187,105), (181,77)]
+    # poppetje7 = [(57,140), (181,77)]
+    # poppetje8 = [(58,105), (181,77)]
+    # poppetje9 = [(70,105), (181,77)]
+    # poppetje10 = [(93, 19), (18,71)]
+    # poppetje11 = [(44,83), (18,71)]
+    # poppetje12 = [(94,37), (181,77)]
+    # poppetje13 = [(128,16), (18,71)]
+    # poppetje14 = [(36,16), (181,77)]
+    # poppetje15 = [(68,17), (18,71)]
+    # poppetje16 = [(188,16), (18,71)]
+    # poppetje17 = [(70,45), (18,71)]
 
     maze = image_to_array()
-
-
-    # path1 = astar(grid, (38,15), (181,78))
-    # start = [35,12]
-    # end = [80,158]
     cost = 1
 
-    path = search(maze,cost, start, end)
-    #path_array = np.array(path)
-    #maxi = np.amax(path_array)
-    
-    #path_list = []
-    #for i in range(maxi + 1):
-    #    point = np.where(path_array==i)
-    #    point= list(zip(point[0], point[1]))
-
-    #    path_list.append(point)
-    #ath2 = astar(grid, (130,17), (18,71))
-    #print(path1)
-    print(path)
+    path = search(maze, cost, start, end)
     return path
+
 
 if __name__ == '__main__':
     main()
