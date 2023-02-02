335b18bfb122bf57e02c0451fad20d3e75d07757
Antonio La Chira <antonio1891@hotmail.com>
2023-02-02 15:14:24 +0100

    Cleanup

--- person_agent.py
+++ person_agent.py
@@ -5,8 +5,6 @@
 """
 
 import mesa
-from generate_arrow_list import generate_arrow_list
-from Blueprints.arrow_priority import arrow_priority
 import a_star as pf
 
 
@@ -15,8 +13,6 @@ EXIT2 = [(61, 33), (62, 33)]
 EXIT3 = [(79, 158), (80, 158), (81, 158), (82, 158), (83, 158)]
 EXIT4 = [(76, 171), (77, 171), (78, 171), (79, 171)]
 ALL_EXITS = EXIT1 + EXIT2 + EXIT3 + EXIT4
-
-ARROWS = generate_arrow_list(ALL_EXITS)
 
 
 class PersonAgent(mesa.Agent):
@@ -27,106 +23,29 @@ class PersonAgent(mesa.Agent):
         # Define state etc. below
         self.pos = pos
         self.end = end
-        self.dir = None
-        self.arrows = ARROWS
-        self.best_ar = None
+        # Calculate path from self.pos to self.end using the A* algorithm
         self.path = pf.main(list(self.pos), list(self.end))
-
-    def _sees_exit(self):
-        # Check if there is an exit in neighborhood with range max_vis (visible
-        # distance in smoke). There's probably an efficient way to check this
-        # instead of just checking all possible neighborhood cells
-        pass
 
     def move(self):
         # If no arrows in visibility range randomwalk, if arrow(s) then find
         # best exit according to distance between arrow and exit and move
         # towards this exit. After passing the arrow keep moving in direction
         # of arrow.
-        # self.model.grid.move_agent(self, next_loc)
-
         next_loc = self.path.pop(0)
         self.model.grid.move_agent(self, next_loc)
 
-    def pos_by_dir(self, direction):
-        neighbor_cells = self.model.grid.get_neighborhood(self.pos, moore=True)
-        possible_empty = [cell for cell in neighbor_cells
-                          if self.model.grid.is_cell_empty(cell)]
-        x, y = self.pos
-        if direction == 0 and (x, y + 1) in possible_empty:
-            return (x, y + 1)
-        elif direction == 1 and (x + 1, y) in possible_empty:
-            return (x + 1, y)
-        elif direction == 2 and (x, y - 1) in possible_empty:
-            return (x, y - 1)
-        elif direction == 3 and (x - 1, y) in possible_empty:
-            return (x - 1, y)
-        return False
-
-    def get_empty_neighborhood(self):
-        # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        pass
-
-    def calc_move(self, loc):
-        possible_moves = []
-        neighbor_cells = self.model.grid.get_neighborhood(self.pos, moore=True)
-        possible_empty = [cell for cell in neighbor_cells
-                          if self.model.grid.is_cell_empty(cell)]
-        x, y = self.pos
-        i, j = loc[0] - x, loc[1] - y
-        if i >= 0 and j >= 0:
-            possible_moves = [(x + 1, y + 1), (x + 1, y), (x, y + 1)]
-        elif i >= 0 and j <= 0:
-            possible_moves = [(x + 1, y - 1), (x + 1, y), (x, y - 1)]
-        elif i <= 0 and j <= 0:
-            possible_moves = [(x - 1, y - 1), (x - 1, y), (x, y - 1)]
-        elif i <= 0 and j >= 0:
-            possible_moves = [(x - 1, y + 1), (x - 1, y), (x, y + 1)]
-        if possible_moves:
-            legal_moves = [dest for dest in possible_moves
-                           if dest in possible_empty]
-            if legal_moves:
-                return self.random.choice(legal_moves)
-        return False
-
-    def find_best_exit(self, arrows):
-        nearest_exit = None
-        exit_dist = 9999
-        for ar in arrows:
-            ar_exit, dist = arrow_priority(ar.exit_priority)
-            if dist < exit_dist:
-                nearest_exit = ar
-                exit_dist = dist
-        return nearest_exit
-
-    def visible_arrows(self):
-        # Collect all arrows visible within range (max_vis)
-        vis_arrows = []
-        for arrow in self.arrows:
-            if arrow.is_in_range(self.pos, self.model.max_vis):
-                vis_arrows.append(arrow)
-        return vis_arrows
-
     def step(self):
-        if self.pos in EXIT1:
+        if self.pos in ALL_EXITS:
+            if self.pos in EXIT1:
+                self.model.counter_EXIT1 += 1
+            elif self.pos in EXIT2:
+                self.model.counter_EXIT2 += 1
+            elif self.pos in EXIT3:
+                self.model.counter_EXIT3 += 1
+            elif self.pos in EXIT4:
+                self.model.counter_EXIT4 += 1
             self.model.grid.remove_agent(self)
             self.model.schedule.remove(self)
             self.model.person_agents -= 1
-            self.model.counter_EXIT1 += 1
-        elif self.pos in EXIT2:
-            self.model.grid.remove_agent(self)
-            self.model.schedule.remove(self)
-            self.model.person_agents -= 1
-            self.model.counter_EXIT2 += 1
-        elif self.pos in EXIT3:
-            self.model.grid.remove_agent(self)
-            self.model.schedule.remove(self)
-            self.model.person_agents -= 1
-            self.model.counter_EXIT3 += 1
-        elif self.pos in EXIT4:
-            self.model.grid.remove_agent(self)
-            self.model.schedule.remove(self)
-            self.model.person_agents -= 1
-            self.model.counter_EXIT4 += 1
         else:
             self.move()
